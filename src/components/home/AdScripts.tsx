851280904162639586f2ccd677a1d77dbfac175b
AkmXStar <108454322+akm12109@users.noreply.github.com>
2025-05-15 17:28:28 +0530

    rem popups

--- src/components/home/AdScripts.tsx
+++ src/components/home/AdScripts.tsx
@@ -6,23 +6,14 @@ import Script from 'next/script';
 export default function AdScripts() {
   return (
     <>
-      {/* Adsterra Script 1 */}
-      <Script
-        id="adsterra-script-1"
-        type="text/javascript"
-        src="//pl26649952.profitableratecpm.com/d3/a9/68/d3a968ac3ebb069aba3cb428edab3a48.js"
-        strategy="lazyOnload"
-        onError={(e) => {
-          console.error('Adsterra script 1 failed to load:', e);
-        }}
-      />
+      {/* Removed Adsterra Script 1 (//pl26649952.profitableratecpm.com/d3/a9/68/d3a968ac3ebb069aba3cb428edab3a48.js) as it was likely causing popups */}
 
-      {/* Adsterra Script 2 (with async and data-cfasync attributes) */}
+      {/* Adsterra Script 2 (for banner in container div) */}
       <Script
         id="adsterra-script-2-invoke"
         src="//pl26649938.profitableratecpm.com/0d34bfa184002fad5912558643501f63/invoke.js"
         strategy="lazyOnload"
-        data-cfasync="false" // next/script handles async implicitly with strategy, but data attributes can be passed
+        data-cfasync="false"
         onError={(e) => {
           console.error('Adsterra script 2 (invoke) failed to load:', e);
         }}
